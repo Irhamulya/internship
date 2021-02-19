f5862216aaa9054e40c777660d51887e50f543d3
irhamulya <irhamuye9@gmail.com>
2021-02-20 01:54:34 +0700

    Revisi

--- intern.sql
+++ intern.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 23 Apr 2020 pada 23.44
+-- Waktu pembuatan: 19 Feb 2021 pada 19.51
 -- Versi server: 10.1.37-MariaDB
 -- Versi PHP: 7.3.0
 
@@ -25,6 +25,29 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Struktur dari tabel `absensi`
+--
+
+CREATE TABLE `absensi` (
+  `id` int(11) NOT NULL,
+  `user_email` varchar(128) NOT NULL,
+  `date` date NOT NULL,
+  `time_in` time NOT NULL,
+  `time_out` time DEFAULT NULL,
+  `overdue` int(11) NOT NULL,
+  `note` varchar(168) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `absensi`
+--
+
+INSERT INTO `absensi` (`id`, `user_email`, `date`, `time_in`, `time_out`, `overdue`, `note`) VALUES
+(204, 'irhamuye911@gmail.com', '2021-02-08', '11:04:06', '11:07:09', 2, NULL);
+
+-- --------------------------------------------------------
+
+--
 -- Struktur dari tabel `history`
 --
 
@@ -34,8 +57,8 @@ CREATE TABLE `history` (
   `nama` varchar(128) NOT NULL,
   `email` varchar(128) NOT NULL,
   `alamat` varchar(128) NOT NULL,
-  `sekolah` varchar(128) NOT NULL,
-  `srt_pkl` varchar(128) NOT NULL,
+  `sekolah` int(11) NOT NULL,
+  `srt_pkl` varchar(128) DEFAULT NULL,
   `tanggal_mulai` date NOT NULL,
   `tanggal_akhir` date NOT NULL,
   `status` int(1) NOT NULL
@@ -51,7 +74,7 @@ CREATE TABLE `ketentuan` (
   `id` int(11) NOT NULL,
   `divisi` varchar(70) NOT NULL,
   `qta` int(11) NOT NULL,
-  `warna` varchar(128) NOT NULL
+  `warna` varchar(128) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -59,8 +82,8 @@ CREATE TABLE `ketentuan` (
 --
 
 INSERT INTO `ketentuan` (`id`, `divisi`, `qta`, `warna`) VALUES
-(1, 'Design Grafis', 0, '#FC9126'),
-(4, 'IT', 2, '#4169E1');
+(1, 'QA', 2, '#00CFE8'),
+(2, 'IT', 2, '#0D4C7F');
 
 -- --------------------------------------------------------
 
@@ -74,12 +97,19 @@ CREATE TABLE `pendaftar` (
   `nama` varchar(128) NOT NULL,
   `email` varchar(128) NOT NULL,
   `alamat` varchar(128) NOT NULL,
-  `sekolah` varchar(128) NOT NULL,
-  `srt_pkl` varchar(128) NOT NULL,
+  `sekolah` int(11) NOT NULL,
+  `srt_pkl` varchar(128) DEFAULT NULL,
   `tanggal_mulai` date NOT NULL,
   `tanggal_akhir` date NOT NULL,
   `status` int(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `pendaftar`
+--
+
+INSERT INTO `pendaftar` (`id`, `id_div`, `nama`, `email`, `alamat`, `sekolah`, `srt_pkl`, `tanggal_mulai`, `tanggal_akhir`, `status`) VALUES
+(1, 2, 'Irham Ulya', 'irhamuye11@gmail.com', 'soetta', 1, NULL, '2021-02-18', '2021-02-26', 0);
 
 -- --------------------------------------------------------
 
@@ -93,20 +123,34 @@ CREATE TABLE `peserta` (
   `nama` varchar(128) NOT NULL,
   `email` varchar(128) NOT NULL,
   `alamat` varchar(128) NOT NULL,
-  `sekolah` varchar(128) NOT NULL,
-  `srt_pkl` varchar(128) NOT NULL,
+  `sekolah` int(11) NOT NULL,
+  `srt_pkl` varchar(128) DEFAULT NULL,
   `tanggal_mulai` date NOT NULL,
   `tanggal_akhir` date NOT NULL,
   `status` int(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
+
 --
--- Dumping data untuk tabel `peserta`
+-- Struktur dari tabel `sekolah`
 --
 
-INSERT INTO `peserta` (`id`, `id_div`, `nama`, `email`, `alamat`, `sekolah`, `srt_pkl`, `tanggal_mulai`, `tanggal_akhir`, `status`) VALUES
-(16, 1, 'Irham Ulya', 'irhamuye9@gmail.com', 'jnklkll', 'SMKN 11', '', '2020-04-02', '2020-04-30', 0),
-(17, 4, 'Irham Ulya', 'irhamuye9@gmail.com', 'adasdas', 'SMKN 11', '', '2020-04-24', '2020-04-30', 0);
+CREATE TABLE `sekolah` (
+  `id` int(11) NOT NULL,
+  `user_email` varchar(128) NOT NULL,
+  `sekolah` varchar(192) NOT NULL,
+  `alamat` varchar(128) NOT NULL,
+  `qty` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `sekolah`
+--
+
+INSERT INTO `sekolah` (`id`, `user_email`, `sekolah`, `alamat`, `qty`) VALUES
+(1, 'irhamuye9@gmail.com', 'SMK 12', 'Jl kemuning', NULL),
+(2, 'irhamuye11@gmail.com', 'SMK 1', 'Baleendah', NULL);
 
 -- --------------------------------------------------------
 
@@ -130,10 +174,10 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `nama`, `email`, `image`, `password`, `role_id`, `is_active`, `date_created`) VALUES
-(1, 'irham ulya', 'irhamuye9@yahoo.com', '31l8KQlPHHL.jpg', '$2y$10$2hTCJVggONDXiDmdOQ2AieEn86v34uZlO5iOeyH232Wc1iABfmjjW', 1, 1, 1569801924),
-(2, 'irham', 'irhamuye911@yahoo.com', 'default.jpg', '$2y$10$mpCuppK7DxyZNPGBpop8huxYKBvp/vN5P6M3QVX45i2hrvCGyT17i', 2, 1, 1570252606),
-(5, 'Irham Ulya', 'irhamuye9@gmail.com', 'default.jpg', '$2y$10$KFyXYoFvfKOau5CJjmrFkOowKmzIt6NP6PtMI9ORuQ/pLWonxdVGy', 2, 1, 1587178670),
-(6, 'Irham Ulya', 'irhamuye11@gmail.com', 'default.jpg', '$2y$10$0WqbsGvhNnXya94boDWEFOyQOhKlgyvDcxLYCFXWMjlgDfO6qjmhq', 2, 1, 1587199959);
+(1, 'Irham Ulya', 'irhamuye9@gmail.com', 'default.jpg', '$2y$10$ec3IdG9Z3JtVG0Y2XfobaeXuoas5LgTLbWvu9cwT9xzO6fFgdCHoS', 1, 1, 1610974680),
+(2, 'Akun test', 'testbaruing@gmail.com', '10743111741548234985-128.png', '$2y$10$m57qqqd20.dXsZ027Dw0se/Qmcf2cF.gf2d8qGQIBMeG2I3U3rloe', 2, 1, 1610974680),
+(3, 'Irham Ulya', 'irhamuye11@gmail.com', '10743111741548234985-128.png', '$2y$10$m57qqqd20.dXsZ027Dw0se/Qmcf2cF.gf2d8qGQIBMeG2I3U3rloe', 4, 1, 1610974680),
+(6, 'Riu', 'irhamuye911@gmail.com', 'default.jpg', '$2y$10$BuYTK/yibInGS9nJHDsU7OjqTCtnM2GuQzs5N2BXmDhwVw8Zw9dSO', 4, 1, 1612718803);
 
 -- --------------------------------------------------------
 
@@ -153,10 +197,19 @@ CREATE TABLE `user_access` (
 
 INSERT INTO `user_access` (`id`, `role_id`, `menu_id`) VALUES
 (1, 1, 1),
-(9, 1, 2),
-(12, 1, 3),
-(13, 1, 4),
-(14, 2, 4);
+(2, 2, 4),
+(3, 1, 4),
+(4, 1, 2),
+(5, 3, 4),
+(8, 1, 5),
+(9, 2, 5),
+(33, 4, 2),
+(35, 4, 4),
+(38, 1, 6),
+(40, 3, 6),
+(41, 2, 6),
+(42, 4, 6),
+(43, 1, 3);
 
 -- --------------------------------------------------------
 
@@ -177,7 +230,9 @@ INSERT INTO `user_menu` (`id`, `menu`) VALUES
 (1, 'Admin'),
 (2, 'User'),
 (3, 'Menu'),
-(4, 'Participant');
+(4, 'Participant'),
+(5, 'Operational'),
+(6, 'Home');
 
 -- --------------------------------------------------------
 
@@ -196,7 +251,8 @@ CREATE TABLE `user_role` (
 
 INSERT INTO `user_role` (`id`, `role`) VALUES
 (1, 'Admin'),
-(2, 'User');
+(2, 'HRD'),
+(4, 'User');
 
 -- --------------------------------------------------------
 
@@ -218,12 +274,16 @@ CREATE TABLE `user_sub_menu` (
 --
 
 INSERT INTO `user_sub_menu` (`id`, `menu_id`, `title`, `url`, `icon`, `is_active`) VALUES
-(1, 1, 'Home', 'admin', 'fas fa-fw fa-home', 1),
-(5, 3, 'Management', 'menu', 'fas fa-fw fa-folder', 1),
-(6, 3, 'Submenu Management', 'menu/submenu', 'fas fa-fw fa-folder-open', 1),
-(7, 2, 'Intern', 'user/peserta', 'fas fa-fw fa-user-graduate', 1),
-(8, 1, 'Role', 'admin/role', 'fas fa-fw fa-users', 1),
-(9, 4, 'Registration', 'participant/register', 'fas fa-fw fa-user-graduate', 1);
+(2, 3, 'Management', 'menu', 'fas fa-fw fa-folder', 1),
+(3, 3, 'Submenu Management', 'menu/submenu', 'fas fa-fw fa-folder-open', 1),
+(4, 2, 'Internship', 'user/peserta', 'fas fa-fw fa-user-graduate', 1),
+(5, 1, 'Role', 'admin/role', 'fas fa-fw fa-users', 1),
+(6, 4, 'Application', 'participant/application', 'fas fa-fw fa-user-graduate', 1),
+(7, 2, 'History', 'user/history', 'fas fas fa-history', 1),
+(8, 2, 'Attendances', 'user/absen', 'fas fa-id-badge', 1),
+(12, 5, 'Division', 'operational/divisi', 'fas fa-user-astronaut', 1),
+(13, 5, 'Worktime', 'operational/worktime', 'fas fa-business-time', 1),
+(14, 4, 'School', 'participant/sekolah', 'fas fa-fw fa-school', 1);
 
 -- --------------------------------------------------------
 
@@ -238,9 +298,35 @@ CREATE TABLE `user_token` (
   `date_created` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `worktime`
+--
+
+CREATE TABLE `worktime` (
+  `id` int(20) NOT NULL,
+  `work_in` time NOT NULL,
+  `work_out` time NOT NULL,
+  `description` varchar(128) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `worktime`
+--
+
+INSERT INTO `worktime` (`id`, `work_in`, `work_out`, `description`) VALUES
+(1, '06:20:00', '19:00:00', 'Jam Magang');
+
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indeks untuk tabel `absensi`
+--
+ALTER TABLE `absensi`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indeks untuk tabel `history`
@@ -264,6 +350,12 @@ ALTER TABLE `pendaftar`
 -- Indeks untuk tabel `peserta`
 --
 ALTER TABLE `peserta`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `sekolah`
+--
+ALTER TABLE `sekolah`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -303,8 +395,20 @@ ALTER TABLE `user_token`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indeks untuk tabel `worktime`
+--
+ALTER TABLE `worktime`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT untuk tabel yang dibuang
 --
+
+--
+-- AUTO_INCREMENT untuk tabel `absensi`
+--
+ALTER TABLE `absensi`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=205;
 
 --
 -- AUTO_INCREMENT untuk tabel `history`
@@ -316,19 +420,25 @@ ALTER TABLE `history`
 -- AUTO_INCREMENT untuk tabel `ketentuan`
 --
 ALTER TABLE `ketentuan`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT untuk tabel `pendaftar`
 --
 ALTER TABLE `pendaftar`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT untuk tabel `peserta`
 --
 ALTER TABLE `peserta`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `sekolah`
+--
+ALTER TABLE `sekolah`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT untuk tabel `user`
@@ -340,31 +450,37 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT untuk tabel `user_access`
 --
 ALTER TABLE `user_access`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
 
 --
 -- AUTO_INCREMENT untuk tabel `user_menu`
 --
 ALTER TABLE `user_menu`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT untuk tabel `user_role`
 --
 ALTER TABLE `user_role`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT untuk tabel `user_sub_menu`
 --
 ALTER TABLE `user_sub_menu`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT untuk tabel `user_token`
 --
 ALTER TABLE `user_token`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `worktime`
+--
+ALTER TABLE `worktime`
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
